06d3798baeeae96177267838f54124a0119b5348
Ivan Alimin <aliminivan1990@gmail.com>
2024-08-15 22:15:20 +0300

    Practical task - Concurrency-multi-threaded bank account
    - fix possible deadlock situation in transfer method of
    ConcurrentBank class

--- src/main/java/com/ivanalimin/concurrent_bank/ConcurrentBank.java
+++ src/main/java/com/ivanalimin/concurrent_bank/ConcurrentBank.java
@@ -22,8 +22,17 @@ public class ConcurrentBank {
         if (fromAccount == null || toAccount == null) {
             throw new IllegalArgumentException("Invalid account number");
         }
-        synchronized (fromAccount) {
-            synchronized (toAccount) {
+        //https://ru.stackoverflow.com/questions/1314103/%D0%97%D0%B0%D1%87%D0%B5%D0%BC-%D0%BD%D1%83%D0%B6%D0%BD%D1%8B-%D0%B2%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%BD%D1%8B%D0%B5-%D0%B1%D0%BB%D0%BE%D0%BA%D0%B8-%D1%81%D0%B8%D0%BD%D1%85%D1%80%D0%BE%D0%BD%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8
+        Object firstLock, secondLock;
+        if (fromAccount.getAccountNumber().compareTo(toAccount.getAccountNumber()) < 0) {
+            firstLock = fromAccount;
+            secondLock = toAccount;
+        } else {
+            firstLock = toAccount;
+            secondLock = fromAccount;
+        }
+        synchronized (firstLock) {
+            synchronized (secondLock) {
                 if (fromAccount.getBalance().compareTo(amount) >= 0) {
                     fromAccount.withdraw(amount);
                     toAccount.deposit(amount);
